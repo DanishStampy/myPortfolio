ada6265da3e79a56a0c6b33d2940378038e936e9
Danish Irfan Shah <64722277+DanishStampy@users.noreply.github.com>
2022-07-26 01:10:39 +0800

    Updates

---
+++ precache-manifest.60da5732eb7229f316340b87088992a9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "925178adbdb117584d10",
+    "url": "/myportfolio/static/css/main.e74e72c5.chunk.css"
+  },
+  {
+    "revision": "925178adbdb117584d10",
+    "url": "/myportfolio/static/js/main.925178ad.chunk.js"
+  },
+  {
+    "revision": "0a69c9f32b85424a05d8",
+    "url": "/myportfolio/static/js/1.0a69c9f3.chunk.js"
+  },
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "a28206c46cc0713a874c2e98cac6dcb8",
+    "url": "/myportfolio/static/media/Danish Irfan - Resume.a28206c4.pdf"
+  },
+  {
+    "revision": "95fc697107767fbd400a8c5073ff7836",
+    "url": "/myportfolio/static/media/profilepic.95fc6971.png"
+  },
+  {
+    "revision": "254db213948a75c0c14391d4da34274a",
+    "url": "/myportfolio/static/media/profilepic_about.254db213.jpg"
+  },
+  {
+    "revision": "16f3610033081d6e6ea381b57d15c256",
+    "url": "/myportfolio/static/media/ukmads.16f36100.png"
+  },
+  {
+    "revision": "e9e596131652483e69a9f7f80b86290c",
+    "url": "/myportfolio/static/media/discord.e9e59613.png"
+  },
+  {
+    "revision": "7dcf9f4b1e5903eb3415069fb1b5298f",
+    "url": "/myportfolio/static/media/flossy2.7dcf9f4b.png"
+  },
+  {
+    "revision": "98febd02b5cf10ec14b60e5e7290a59f",
+    "url": "/myportfolio/static/media/giftlogo.98febd02.png"
+  },
+  {
+    "revision": "c3bf8ef792d874334d5d177b1b2632d4",
+    "url": "/myportfolio/index.html"
+  }
+];
